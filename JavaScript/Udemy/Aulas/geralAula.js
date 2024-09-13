636311f1c868e649d04316dc073b87c63b6a8caa
Sinval Silva Junior <sinvaljuniorlms@gmail.com>
2024-09-13 16:56:56 -0300

    Atualização

---
+++ JavaScript/Udemy/Aulas/geralAula.js
@@ -0,0 +1,5 @@
+let num = 10;
+console.log("Olá, mundo!");
+console.log(num, `"Teste" do 'Olá mundo'!`);
+console.log("Para comentar uma linha, usa-se //COMENTÁRIO");
+console.log("Para comentar várias linhas usa-se /* COMENTÁRIO */");

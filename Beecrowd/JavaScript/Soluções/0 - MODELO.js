b85139ca77555e94256029cf7f478e52e7be1483
kjunda01 <sinvaljuniorlms@gmail.com>
2024-09-15 19:56:50 -0300

    Alterações importantes e alguns exercícios

---
+++ Beecrowd/JavaScript/Soluções/0 - MODELO.js
@@ -0,0 +1,11 @@
+export function problem(lines) {
+    //var input = require("fs").readFileSync("/dev/stdin", "utf8");
+    //var lines = input.split("\n");
+    let entrada = lines[0];
+
+    /*
+    lines.forEach((elemento) => {
+        console.log(elemento);
+    });
+    */
+}
---
+++ Beecrowd/JavaScript/Soluções/1000.js
@@ -0,0 +1,7 @@
+export function problem(lines) {
+    //var input = require("fs").readFileSync("/dev/stdin", "utf8");
+    //var lines = input.split("\n");
+    //let entrada = lines[0];
+
+    console.log(`Hello World!`)
+}

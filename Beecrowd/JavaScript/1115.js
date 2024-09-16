c0a508a7c3a98512e724405d4f2d34c385ef5947
kjunda01 <sinvaljuniorlms@gmail.com>
2024-09-16 13:00:36 -0300

    1115

--- Beecrowd/JavaScript/1115.js
+++ Beecrowd/JavaScript/1115.js
@@ -33,23 +33,27 @@ var lines = input.split("\n");
 
 let tamanho = lines.length;
 
-function quadrante(pontoX, pontoY) {
-    if (pontoX > 0 && pontoY > 0) {
-        console.log("primeiro")
-    } else if ( pontoX < 0 && pontoY > 0) {
-        console.log("segundo")
-
-
-    }
-}
-
-
 for (let i = 0; i < tamanho; i++) {
     let partes = lines[i].split(" ");
-    if (partes.length > 1) {
-        let x = Number(partes[0], 10); // Primeiro número
-        let y = Number(partes[1], 10); // Segundo número
-        // Imprime os valores individuais
-        console.log(`x: ${x}, y: ${y}`);
+
+    let x = Number(partes[0]);
+    let y = Number(partes[1]);
+
+    if (x > 0) {
+        if (y > 0) {
+            console.log("primeiro");
+        } else if (y < 0) {
+            console.log("quarto");
+        } else {
+            console.log("");
+        }
+    } else if (x < 0) {
+        if (y > 0) {
+            console.log("segundo");
+        } else if (y < 0) {
+            console.log("terceiro");
+        } else {
+            console.log("");
+        }
     }
 }

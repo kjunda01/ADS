573bf6790201401dddb29ee266916764dbbeae57
Sinval Silva Junior <sinvaljuniorlms@gmail.com>
2024-09-20 14:51:07 -0300

    28. Exercícios com numbers e Math

--- JavaScript/Udemy/Aulas/28. Exercícios com numbers e Math.js
+++ JavaScript/Udemy/Aulas/28. Exercícios com numbers e Math.js
@@ -4,4 +4,4 @@ const numeroTitulo = document.getElementById("numero-titulo");
 const texto = document.getElementById("texto");
 
 numeroTitulo.innerHTML = numero;
-texto.innerHTML = `<p>Seu número - 2 é ${numero - 2}<p/>`
+texto.innerHTML += `<p>Seu número - 2 é ${numero - 2}<p/>`

480379ffb4446dc9ea72010b54b68a64cc2e27e5
kjunda01 <sinvaljuniorlms@gmail.com>
2024-09-21 19:47:03 -0300

    Criação de modelo HTML/CSS/JS

--- JavaScript/Udemy/Aulas/geralAula.js
+++
@@ -1,5 +0,0 @@
-let num = 10;
-console.log("Olá, mundo!");
-console.log(num, `"Teste" do 'Olá mundo'!`);
-console.log("Para comentar uma linha, usa-se //COMENTÁRIO");
-console.log("Para comentar várias linhas usa-se /* COMENTÁRIO */");

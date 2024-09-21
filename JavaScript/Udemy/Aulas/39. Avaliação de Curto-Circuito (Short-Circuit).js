3bdac1d00775cc413b94b499fc686aa072b154b5
kjunda01 <sinvaljuniorlms@gmail.com>
2024-09-21 18:44:50 -0300

    39. Avaliação de Curto-Circuito (Short-Circuit)

---
+++ JavaScript/Udemy/Aulas/39. Avaliação de Curto-Circuito (Short-Circuit).js
@@ -0,0 +1,15 @@
+/*
+&& -> SÓ FUNCIONA SE TODAS AS VALIDAÇÕES FOREM VERDADEIRAS
+|| -> FUNCIONA SE UMA DAS VALIDAÇÕES FOR VERDADEIRA
+*/
+
+
+/*
+VALORES AVALIADOS COMO FALSY(Valores falsos)
+*false(esse valor é literal)
+0
+'' "" ``
+null / undefined
+NaN
+*/
+

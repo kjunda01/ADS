1dddd3ebe4f963889a8bd5b3188e5a4d833055eb
kjunda01 <sinvaljuniorlms@gmail.com>
2024-09-20 21:50:34 -0300

    31. Funções em JavaScript - Uma introdução ao básico

--- JavaScript/Udemy/Aulas/31. Funções em JavaScript - Uma introdução ao básico.js
+++ JavaScript/Udemy/Aulas/31. Funções em JavaScript - Uma introdução ao básico.js
@@ -1,5 +1,30 @@
-function saudacao(nome) {
+function saudacao() {
+    console.log("Olá, mundo!");
+}
+saudacao();
+
+function saudacaoComArgumento(nome) {
     console.log(nome);
 }
-nome = "Olá, mundo!"
-saudacao(nome);
+
+saudacaoComArgumento("Olá mundo novamente!");
+
+function somaDoisValores(primeiroValor = 1, segundoValor = 1) {
+    const resultado = primeiroValor + segundoValor;
+    return resultado;
+}
+
+console.log(`A soma dos dois valores é: ${somaDoisValores(1, 3)}`);
+
+// FORMA PARA CHAMAR FUNÇÃO USANDO UMA VARIÁVEL
+const raiz = function (n) {
+    return n ** 0.5;
+};
+
+console.log(raiz(9));
+console.log(raiz(16));
+
+// FORMA PARA CHAMAR FUNÇÃO USANDO ARROW FUNCTIONS (=>)
+const multiplicaPor3 = (n) => n * 3;
+
+console.log(multiplicaPor3(25));

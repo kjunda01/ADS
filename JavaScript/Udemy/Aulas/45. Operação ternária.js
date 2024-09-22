d50da22f9888f018e5e9ecb80d9fa85460e24a92
kjunda01 <sinvaljuniorlms@gmail.com>
2024-09-22 17:29:12 -0300

    45. Operação ternária

---
+++ JavaScript/Udemy/Aulas/45. Operação ternária.js
@@ -0,0 +1,14 @@
+// Operadores ternarios são (? e :)
+
+// CONDIÇÃO ? VALOR-PARA-VERDADEIRO : VALOR-PARA-FALSO
+// (condição) ? (Valor para verdade) : (Valor para falso)
+
+const pontuacaoUsuario = 1000;
+pontuacaoUsuario >= 1000 ? console.log("Usuario VIP") : console.log("Usuario normal")
+
+// OU
+
+pontuacaoUsuario >= 1000
+    ? console.log("Usuario VIP")
+    : console.log("Usuario normal");
+

9dbdbb82d8ea34a6b1eac042bb6b3890c265c739
Sinval Silva Junior <sinvaljuniorlms@gmail.com>
2024-10-02 10:21:46 -0300

    Atualizações

--- JavaScript/Udemy/Aulas/Exercicios/67. Criando uma Lista de tarefas/Website/assets/js/main.js
+++ JavaScript/Udemy/Aulas/Exercicios/67. Criando uma Lista de tarefas/Website/assets/js/main.js
@@ -1,10 +1,15 @@
+/*
 const botaoAdicionarTarefa = document.querySelector(".btnAddTarefa");
 const textoNovaTarefa = document.querySelector(".inputText");
 const btnApagar = document.querySelector(".btnApagar");
 const div = document.querySelector(".itens");
 
-function createNewInput(msg) {
+function limpaTarefa() {
+    textoNovaTarefa.value = " ";
+    textoNovaTarefa.focus();
+}
 
+function createNewInput(msg) {
     // Cria o parágrafo
     const lista = document.createElement("li");
     const nodeTxt = document.createTextNode(msg);
@@ -17,8 +22,20 @@ function createNewInput(msg) {
     const nodeBtn = document.createTextNode("Apagar");
     botao.appendChild(nodeBtn);
     botao.classList.add("btnApagar");
+    botao.setAttribute('title', 'Apagar esta tarefa');
     paragrafo.appendChild(botao);
 }
+
+textoNovaTarefa.addEventListener("keypress", function (e) {
+    let texto = textoNovaTarefa.value;
+    if (e.key === "Enter") {
+        createNewInput(texto);
+        limpaTarefa();
+    }
+    if (!texto === "") {
+        alert("Texto não pode ser vazio!");
+    }
+});
 
 // Capta todos os cliques da tela
 document.addEventListener("click", function (e) {
@@ -29,8 +46,7 @@ document.addEventListener("click", function (e) {
     if (elementoClicado === botaoAdicionarTarefa) {
         if (texto) {
             createNewInput(texto);
-            textoNovaTarefa.value = ""; // Limpa o campo de texto
-            // Falta limpar o campo do texto
+            limpaTarefa();
         } else {
             alert("Texto não pode ser vazio!");
         }
@@ -45,3 +61,81 @@ document.addEventListener("click", function (e) {
         }
     }
 });
+*/
+const inputTarefa = document.querySelector(".input-tarefa");
+const btnTarefa = document.querySelector(".btn-tarefa");
+const tarefas = document.querySelector(".tarefas");
+
+function criaLi() {
+    const li = document.createElement("li");
+    return li;
+}
+
+inputTarefa.addEventListener("keypress", function (e) {
+    if (e.keyCode === 13) {
+        if (!inputTarefa.value) return;
+        criaTarefa(inputTarefa.value);
+    }
+});
+
+function limpaInput() {
+    inputTarefa.value = "";
+    inputTarefa.focus();
+}
+
+function criaBotaoApagar(li) {
+    li.innerText += " ";
+    const botaoApagar = document.createElement("button");
+    botaoApagar.innerText = "Apagar";
+    // botaoApagar.classList.add('apagar');
+    botaoApagar.setAttribute("class", "apagar");
+    botaoApagar.setAttribute("title", "Apagar esta tarefa");
+    li.appendChild(botaoApagar);
+}
+
+function criaTarefa(textoInput) {
+    const li = criaLi();
+    li.innerText = textoInput;
+    tarefas.appendChild(li);
+    limpaInput();
+    criaBotaoApagar(li);
+    salvarTarefas();
+}
+
+btnTarefa.addEventListener("click", function () {
+    if (!inputTarefa.value) return;
+    criaTarefa(inputTarefa.value);
+});
+
+document.addEventListener("click", function (e) {
+    const el = e.target;
+
+    if (el.classList.contains("apagar")) {
+        el.parentElement.remove();
+        salvarTarefas();
+    }
+});
+
+function salvarTarefas() {
+    const liTarefas = tarefas.querySelectorAll("li");
+    const listaDeTarefas = [];
+
+    for (let tarefa of liTarefas) {
+        let tarefaTexto = tarefa.innerText;
+        tarefaTexto = tarefaTexto.replace("Apagar", "").trim();
+        listaDeTarefas.push(tarefaTexto);
+    }
+
+    const tarefasJSON = JSON.stringify(listaDeTarefas);
+    localStorage.setItem("tarefas", tarefasJSON);
+}
+
+function adicionaTarefasSalvas() {
+    const tarefas = localStorage.getItem("tarefas");
+    const listaDeTarefas = JSON.parse(tarefas);
+
+    for (let tarefa of listaDeTarefas) {
+        criaTarefa(tarefa);
+    }
+}
+adicionaTarefasSalvas();

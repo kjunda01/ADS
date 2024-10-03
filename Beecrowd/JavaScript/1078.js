77e42c387ea3e8cde50681dd26555005f6efe9f5
Sinval Silva Junior <sinvaljuniorlms@gmail.com>
2024-10-03 08:26:43 -0300

    1078

---
+++ Beecrowd/JavaScript/1078.js
@@ -0,0 +1,37 @@
+/*
+ beecrowd | 1078
+Tabuada
+
+Adaptado por Neilor Tonin, URI Brasil
+Timelimit: 1
+
+Leia 1 valor inteiro N (2 < N < 1000). A seguir, mostre a tabuada de N:      
+1 x N = N      2 x N = 2N        ...       10 x N = 10N
+Entrada
+
+A entrada contém um valor inteiro N (2 < N < 1000).
+Saída
+
+Imprima a tabuada de N, conforme o exemplo fornecido.
+Exemplo de Entrada 	Exemplo de Saída
+
+140
+	
+
+1 x 140 = 140
+2 x 140 = 280
+3 x 140 = 420
+4 x 140 = 560
+5 x 140 = 700
+6 x 140 = 840
+7 x 140 = 980
+8 x 140 = 1120
+9 x 140 = 1260
+10 x 140 = 1400 
+*/
+var input = require("fs").readFileSync("/dev/stdin", "utf8").trim();
+var lines = input.split("\n");
+
+for (let i = 1; i <= 10; i++) {
+    console.log(`${i} x ${lines[0]} = ${i * lines[0]}`);
+}

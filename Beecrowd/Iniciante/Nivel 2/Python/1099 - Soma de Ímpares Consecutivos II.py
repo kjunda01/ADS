d276aa3f70a85d463ab2e3fbdc3cbb0c6ba74c34
Sinval Silva Junior <sinvaljuniorlms@gmail.com>
2024-10-08 16:56:05 -0300

    1099 - Soma de Ímpares Consecutivos II - Incompleto

---
+++ Beecrowd/Iniciante/Nivel 2/Python/1099 - Soma de Ímpares Consecutivos II.py
@@ -0,0 +1,60 @@
+'''
+ beecrowd | 1099
+Soma de Ímpares Consecutivos II
+
+Adaptado por Neilor Tonin, URI Brasil
+Timelimit: 1
+
+Leia um valor inteiro N que é a quantidade de casos de teste que vem a seguir. Cada caso de teste consiste de dois inteiros X e Y. Você deve apresentar a soma de todos os ímpares existentes entre X e Y.
+Entrada
+
+A primeira linha de entrada é um inteiro N que é a quantidade de casos de teste que vem a seguir. Cada caso de teste consiste em uma linha contendo dois inteiros X e Y.
+Saída
+
+Imprima a soma de todos valores ímpares entre X e Y.
+Exemplo de Entrada 	Exemplo de Saída
+
+7
+
+4 5
+
+13 10
+
+6 4
+
+3 3
+
+3 5
+
+3 4
+
+3 8
+	
+
+0
+
+11
+
+5
+
+0
+
+0
+
+0
+
+12
+'''
+#casosTeste = int(input())
+casosTeste = 2
+linhas = input().split(" ")
+paraOrdenar = []
+impar = 0
+
+for i in range(casosTeste-1):
+    linhas = list(map(int, input().split(" ")))
+
+for numeros in linhas:
+    if numeros % 2 != 0:
+        impar += numeros
+    print(impar)

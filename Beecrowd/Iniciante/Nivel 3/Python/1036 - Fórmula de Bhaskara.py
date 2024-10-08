21789c40c9c561aa956be837dfd2ed079788e97b
Sinval Silva Junior <sinvaljuniorlms@gmail.com>
2024-10-08 14:15:31 -0300

    1036 - Fórmula de Bhaskara

--- Beecrowd/Iniciante/Nivel 3/Python/1036 - Fórmula de Bhaskara.py
+++ Beecrowd/Iniciante/Nivel 3/Python/1036 - Fórmula de Bhaskara.py
@@ -8,21 +8,3 @@ Saída
 Se não houver possibilidade de calcular as raízes, apresente a mensagem "Impossivel calcular". Caso contrário, imprima o resultado das raízes com 5 dígitos após o ponto, com uma mensagem correspondente conforme exemplo abaixo. Imprima sempre o final de linha após cada mensagem.
 '''
 
-from math import sqrt
-
-
-valoresInformados = '2 -6 0'
-
-listaDosValores = valoresInformados.split(' ')
-
-a = float(listaDosValores[0])
-b = float(listaDosValores[1])
-c = float(listaDosValores[2])
-
-delta = (b ** 2) - (4 * a * (c))
-print(delta)
-
-# r1 = 
-# r2 = 
-
-print(f'R1 = {r1:.5f}\nR2 = {r2:.5f}')

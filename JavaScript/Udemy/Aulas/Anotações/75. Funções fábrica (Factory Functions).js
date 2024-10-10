eeec263dd5a9dd6e433bdd528a3b1fbbee2549fc
kjunda01 <sinvaljuniorlms@gmail.com>
2024-10-10 20:50:17 -0300

    75. Funções fábrica (Factory Functions) - versao 2

--- JavaScript/Udemy/Aulas/Anotações/75. Funções fábrica (Factory Functions).js
+++ JavaScript/Udemy/Aulas/Anotações/75. Funções fábrica (Factory Functions).js
@@ -1,5 +1,7 @@
-// factory function
+const pesoAleatorio = () => Math.random() * 21;
+const alturaAleatoria = () => Math.random() * 2;
 
+// factory function
 function criaPessoa(nome, sobrenome, altura, peso) {
     return {
         nome,
@@ -9,16 +11,16 @@ function criaPessoa(nome, sobrenome, altura, peso) {
         },
         altura,
         peso,
-        imc() {
+        get imc() {
             return (this.peso / this.altura ** 2).toFixed(2);
         },
     };
 }
 
-const p1 = criaPessoa("Giovana", "Morais", 1.8, 80);
-const p2 = criaPessoa("Maria", "Joaquina", 1.6, 42);
+const p1 = criaPessoa("Giovana", "Morais", alturaAleatoria(), pesoAleatorio());
+const p2 = criaPessoa("Maria", "Joaquina", alturaAleatoria(), pesoAleatorio());
 
 console.log(p1.fala("Dizendo Oi."));
 console.log(p2.fala("falando sobre JS."));
-console.log(p1.imc());
-console.log(p2.imc());
+console.log(p1.imc);
+console.log(p2.imc);

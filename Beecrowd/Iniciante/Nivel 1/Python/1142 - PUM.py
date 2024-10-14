f8a83841d51dea3b222f9123d0fd49b895d59a98
kjunda01 <sinvaljuniorlms@gmail.com>
2024-10-14 20:30:33 -0300

    1142 - PUM

---
+++ Beecrowd/Iniciante/Nivel 1/Python/1142 - PUM.py
@@ -0,0 +1,37 @@
+'''
+ beecrowd | 1142
+PUM
+
+Adaptado por Neilor Tonin, URI Brasil
+Timelimit: 1
+
+Escreva um programa que leia um valor inteiro N. Este N é a quantidade de linhas de saída que serão apresentadas na execução do programa.
+Entrada
+
+O arquivo de entrada contém um número inteiro positivo N.
+Saída
+
+Imprima a saída conforme o exemplo fornecido.
+Exemplo de Entrada 	Exemplo de Saída
+
+7
+	
+
+1 2 3 PUM
+5 6 7 PUM
+9 10 11 PUM
+13 14 15 PUM
+17 18 19 PUM
+21 22 23 PUM
+25 26 27 PUM 
+'''
+linhas = int(input())
+
+contador = 1
+for linha in range(linhas):    
+    for i in range(contador, contador+4):
+        if i % 4 == 0:
+            i = 'PUM'
+        print(i, end=(' '))
+    contador += 4
+    print()

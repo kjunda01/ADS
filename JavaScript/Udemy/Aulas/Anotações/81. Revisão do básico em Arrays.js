5871c9fcf39741cd553d268933470e8850ef9396
Sinval Silva Junior <sinvaljuniorlms@gmail.com>
2024-10-15 10:41:07 -0300

    81. Revisão do básico em Arrays

--- JavaScript/Udemy/Aulas/Anotações/81. Revisão do básico em Arrays.js
+++ JavaScript/Udemy/Aulas/Anotações/81. Revisão do básico em Arrays.js
@@ -1,2 +1,8 @@
 //                0          1        2
 const nomes = ['Eduardo', 'Maria', 'Joana']
+// Sytring, Objetos, Funções, Números
+// Pode usar o construtor
+// const nomes = new Array('Eduardo', 'Maria', 'Joana')
+nomes[2] = 'João';
+delete nomes[2];
+console.log(nomes);

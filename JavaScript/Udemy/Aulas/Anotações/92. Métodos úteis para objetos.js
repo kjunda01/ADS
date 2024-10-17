5fded1bedc7916753b2fa190b14491910d272497
kjunda01 <sinvaljuniorlms@gmail.com>
2024-10-17 19:34:21 -0300

    92. Métodos úteis para objetos

--- JavaScript/Udemy/Aulas/Anotações/92. Métodos úteis para objetos.js
+++ JavaScript/Udemy/Aulas/Anotações/92. Métodos úteis para objetos.js
@@ -1,8 +1,8 @@
 /*
 Object.values
 Object.entries
-Object.assign(des, any)
-Object.getOwnPropertyDescriptor(o, "prop")
+Object.assign(destino, varios-outros-objetos)
+Object.getOwnPropertyDescriptor(objeto, "propriedade")
 ... (spread)
 
 // ja vimos
@@ -16,16 +16,20 @@ Object.defineProperty (define uma propriedade)
 // const produto = { nome: "produto", preco: 1.8 };
 // // forma facil de fazer uma cópia de um objeto
 // // usa-se o spread operator { ...NOME-DO-OBJETO}
-const produto = { nome: "produto", preco: 1.8 };
-//const caneca = { ...produto, material: "Porcelana" };
-const caneca = Object.assign({}, produto, { material: "porcelana" });
-Object.freeze(caneca);
+// const produto = { nome: "produto", preco: 1.8 };
+// //const caneca = { ...produto, material: "Porcelana" };
+// const caneca = Object.assign({}, produto, { material: "porcelana" });
+// Object.freeze(caneca);
 
-caneca.nome = "Outro nome";
-caneca.preco = 2.5;
-console.log(produto);
-console.log(caneca);
+// caneca.nome = "Outro nome";
+// caneca.preco = 2.5;
+// console.log(produto);
+// console.log(caneca);
 
-console.log(Object.keys(produto));
-console.log(Object.values(produto));
-console.log(Object.entries(produto));
+// console.log(Object.keys(produto));
+// console.log(Object.values(produto));
+// console.log(Object.entries(produto));
+const produto = { nome: "produto", preco: 1.8, material: "porcelana" };
+for (let [chave, valor] of Object.entries(produto)) {
+    console.log(chave, valor);
+}

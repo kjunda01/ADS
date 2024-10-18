14a159fbb99c37a8536dd083f01c7ef22eec89f8
Sinval Silva Junior <sinvaljuniorlms@gmail.com>
2024-10-18 08:32:31 -0300

    1113 - Crescente e Decrescente

--- Beecrowd/Iniciante/Nivel 1/JavaScript/1113 - Crescente e Decrescente.js
+++ Beecrowd/Iniciante/Nivel 1/JavaScript/1113 - Crescente e Decrescente.js
@@ -1,18 +1,28 @@
 var input = require("fs").readFileSync("./dev/stdin", "utf8").trim();
 var linha = input.split(/\r?\n/);
 
-function criaMatriz(linha) {
-    for (let i in linha) {
-        let temp = [];
+// map com arrow functions
+// let matriz = linha.map((linha) => {
+//     return linha.split(" ").map(Number);
+// });
 
-        //console.log(`Linha -> ${i}`);
-        for (let j in linha[i]) {
-            //console.log(`Coluna: ${j}`);
-            const e = parseInt(linha[i][j].split(" "));
-            if (e) {
-                temp.push(e);
-            }
+// map com função anonima
+let matriz = linha.map(function (linha) {
+    return linha.split(" ").map(Number);
+});
+
+matriz.forEach(function (elemento) {
+    // associação por desestruturação
+    let [primeiro, segundo] = elemento;
+
+    // se os dois numeros forem iguais, encerra o programa
+    if (primeiro === segundo) {
+    } else {
+        // continua a lógica para as mensagens
+        if (primeiro > segundo) {
+            console.log("Decrescente");
+        } else if (primeiro < segundo) {
+            console.log("Crescente");
         }
-        return temp;
     }
-}
+});

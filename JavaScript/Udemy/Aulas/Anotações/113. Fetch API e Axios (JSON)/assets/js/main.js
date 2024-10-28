ebd9b6c3bfd642eede1a96eaf8a83ac1d7ba4cd0
kjunda01 <sinvaljuniorlms@gmail.com>
2024-10-28 16:47:37 -0300

    113. Fetch API e Axios (JSON)

--- JavaScript/Udemy/Aulas/Anotações/113. Fetch API e Axios (JSON)/assets/js/main.js
+++ JavaScript/Udemy/Aulas/Anotações/113. Fetch API e Axios (JSON)/assets/js/main.js
@@ -1,41 +1,38 @@
-const request = (objeto) => {
-    return new Promise((resolve, reject) => {
-        const xhr = new XMLHttpRequest();
-        xhr.open(objeto.method, objeto.url, true);
-        xhr.send();
-        xhr.addEventListener("load", () => {
-            const resposta =
-                xhr.status >= 200 && xhr.status < 300
-                    ? resolve(xhr.responseText)
-                    : reject(xhr.statusText);
-        });
-    });
-};
+// API DOS PAÍSES = 'https://restcountries.com/v3.1/all')
+// API TESTE  = 'https://jsonplaceholder.typicode.com/todos/1'
+//// fetch("assets/json/pessoas.json")
+////     .then((response) => response.json())
+////   .then((json) => carregaElementosNaPagina(json));
 
-document.addEventListener("click", (evento) => {
-    const elemento = evento.target;
-    const tag = elemento.tagName.toLowerCase();
-    if (tag === "a") {
-        evento.preventDefault();
-        carregaPagina(elemento);
+axios("assets/json/pessoas.json")
+.then((response) =>
+    carregaElementosNaPagina(response.data)
+);
+
+function carregaElementosNaPagina(json) {
+    const table = document.createElement("table");
+
+    for (let pessoa of json) {
+        const criaLinha = (campo) => {
+            let td = document.createElement("td");
+            td.innerHTML = campo;
+            tr.appendChild(td);
+        };
+
+        const tr = document.createElement("tr");
+
+        criaLinha(pessoa.nome);
+        criaLinha(pessoa.email);
+        criaLinha(pessoa.salario);
+        criaLinha(pessoa.estado);
+        criaLinha(pessoa.empresa);
+        criaLinha(pessoa.idade);
+        criaLinha(pessoa.sexo);
+        criaLinha(pessoa.cpf);
+
+        table.appendChild(tr);
     }
-});
 
-async function carregaPagina(elemento) {
-    const href = elemento.getAttribute("href");
-    const objConfig = {
-        method: "GET",
-        url: href,
-    };
-    try {
-        const response = await request(objConfig);
-        carregaResultado(response);
-    } catch (erro) {
-        console.log(erro);
-    }
-}
-
-function carregaResultado(response) {
     const resultado = document.querySelector(".resultado");
-    resultado.innerHTML = response;
+    resultado.appendChild(table);
 }

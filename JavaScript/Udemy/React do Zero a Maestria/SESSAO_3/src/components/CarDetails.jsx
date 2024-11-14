95578dd77b9701712558f3b96f797a8db2794b64
kjunda01 <sinvaljuniorlms@gmail.com>
2024-11-13 21:31:38 -0300

    A prop children

--- JavaScript/Udemy/React do Zero a Maestria/SESSAO_3/src/components/CarDetails.jsx
+++ JavaScript/Udemy/React do Zero a Maestria/SESSAO_3/src/components/CarDetails.jsx
@@ -1,14 +1,25 @@
 import React from "react";
+import styles from "./CarDetails.module.css";
 
-const CarDetails = ({ brand, km, color }) => {
+const CarDetails = ({ brand, km, color, newCar }) => {
     return (
-        <div>
-            <h2>Detalhes do Carro</h2>
-            <ul>
-                <li>Marca: {brand}</li>
-                <li>KM: {km}</li>
-                <li>Cor: {color}</li>
-            </ul>
+        <div className={styles.container}>
+            <h2 className={styles.title}>Detalhes do Carro</h2>
+            <table className={styles.table}>
+                <tr>
+                    <th className={styles.th}>Marca</th>
+                    <th className={styles.th}>Km</th>
+                    <th className={styles.th}>Cor</th>
+                    <th className={styles.th}>Carro Novo</th>
+                </tr>
+                <tr>
+                    <td className={styles.td}>{brand}</td>
+                    <td className={styles.td}>{km}</td>
+                    <td className={styles.td}>{color}</td>
+                    <td className={styles.td}>{newCar ? "Sim" : "Não"}</td>
+                </tr>
+            </table>
+            <div className={styles.footer}>Todos os direitos reservados.</div>
         </div>
     );
 };

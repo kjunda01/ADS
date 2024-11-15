f9dab43cfcd2dd0a1045f03bf28e8faa7f1f53e8
kjunda01 <sinvaljuniorlms@gmail.com>
2024-11-15 10:28:28 -0300

    Sessão 4 - Finalizando

---
+++ JavaScript/Udemy/React do Zero a Maestria/SESSAO_4/src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import styles from "./App.module.css";
+
+function App() {
+    return (
+        <div>
+            <h1 className={styles.Titulo}>CSS e React</h1>
+        </div>
+    );
+}
+
+export default App;

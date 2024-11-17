5e799576c51a9152100123c22c0d262ffa8a9e0f
kjunda01 <sinvaljuniorlms@gmail.com>
2024-11-17 19:57:33 -0300

    Algumas mudanças e curso PedroTech

---
+++ JavaScript/Front Beginners/src/components/Button.jsx
@@ -0,0 +1,13 @@
+import React, { useState } from "react";
+
+const Button = () => {
+    const [openModal, setOpenModal] = useState(false);
+    
+    return (
+        <div>
+            <button onClick={() => setOpenModal(true)}>Abrir modal</button>
+        </div>
+    );
+};
+
+export default Button;
---
+++ JavaScript/PedroTech/src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import AdicionarTarefa from "./AdicionarTarefa";
+
+function App() {
+    return (
+        <div>
+            <AdicionarTarefa />
+        </div>
+    );
+}
+
+export default App;

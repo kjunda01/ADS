2288e894f770b40ca2fa9e38891d6e40afafb14a
kjunda01 <sinvaljuniorlms@gmail.com>
2024-12-07 20:48:30 -0300

    Curso de Java #7 - Operadores Aritiméticos e Classe Math

---
+++ Java/CursoEmVideoJava/CursoEmVideo/src/Aula07.java
@@ -0,0 +1,20 @@
+public class Aula07 {
+    public static void main(String[] args){
+        /*
+        Operadores aritiméticos
+        Adição = 5 + 2 = 7
+        Subtração = 5-2 = 3
+        Multiplicação = 5 * 2 = 10
+        Divisão = 5 / 2 = 2.5
+        Resto = 5 % 2 = 1
+
+
+         */
+        float numero1 = 3;
+        float numero2 = 5;
+        float media = (numero1 + numero2) / 2;
+        System.out.println("A média é: " + media);
+
+    }
+}
+

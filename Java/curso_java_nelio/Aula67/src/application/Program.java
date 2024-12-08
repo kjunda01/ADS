a90b6db2801f05f3ea0e768cf8b0ae53338ac98c
kjunda01 <sinvaljuniorlms@gmail.com>
2024-12-08 19:52:33 -0300

    67. Começando a resolver um segundo problema exemplo

--- Java/curso_java_nelio/Aula67/src/application/Program.java
+++ Java/curso_java_nelio/Aula67/src/application/Program.java
@@ -1,11 +1,33 @@
 package application;
 
+import java.util.Locale;
+import java.util.Scanner;
+
+import entities.Product;
+
 public class Program {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Teste");
+	Locale.setDefault(Locale.US);
+	Scanner sc = new Scanner(System.in);
+	
+	Product product = new Product();
+	
+	//	Começa o programa
+	System.out.println("Enter product data: ");
+	
+	System.out.print("Name: ");
+	product.name = sc.nextLine();
+	 
+	System.out.print("Price: ");
+	product.price = sc.nextDouble();
+	
+	System.out.print("Quantity in stock: ");
+	product.quantity = sc.nextInt();
+	
+	
 
+	sc.close();
 	}
 
 }
--- Java/curso_java_nelio/Aula67/src/entities/Product.java
+++ Java/curso_java_nelio/Aula67/src/entities/Product.java
@@ -1,5 +1,21 @@
 package entities;
 
 public class Product {
-
+	//Atributos
+	public String name;
+	public double price;
+	public int quantity;
+	
+	// Métodos
+	public double totalValueInStock() {
+		return price * quantity;
+	}
+	
+	public void addProducts(int quantity) {
+		this.quantity += quantity;
+	}
+	
+	public void removeProducts(int quantity) {
+		this.quantity -= quantity;
+	}
 }

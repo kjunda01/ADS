3da9a3bcf3d4ef8c06c8c0ea6134d0db5488bdad
Sinval Silva Junior <sinvaljuniorlms@gmail.com>
2024-12-13 16:43:42 -0300

    Curso Java Matheus Battisti

---
+++ Java/JavaMatheusBattisti/SECAO_DOIS/PRIMEIRO_PROGRAMA/OlaMundo.java
@@ -0,0 +1,13 @@
+package SECAO_DOIS.PRIMEIRO_PROGRAMA;
+
+public class OlaMundo {
+
+    public static void main(String[] args){
+
+        System.out.println();
+        System.out.println("Versão do Java: " + System.getProperty("java.version"));
+        System.out.println("Olá, mundo!");
+
+
+    }
+}

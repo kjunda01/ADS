50a42429ba0e64ef1930df06d7efb0d86c735600
kjunda01 <sinvaljuniorlms@gmail.com>
2024-12-14 13:00:22 -0300

    Correções em pastas e aulas 76. Construtores, 77. Palavra this, 78. Sobrecarga

---
+++ Java/curso_java_nelio/aula_76_77_78/application/Program.java
@@ -0,0 +1,55 @@
+package application;
+
+import java.util.Locale;
+import java.util.Scanner;
+import entities.Product;
+
+public class Program {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        // Começa o programa
+        System.out.println("Enter product data: ");
+        
+
+        // Pede os dados
+        System.out.print("Name: ");
+        String name = sc.nextLine();
+
+        System.out.print("Price: ");
+        double price = sc.nextDouble();
+
+        System.out.print("Quantity in stock: ");
+        int quantity = sc.nextInt();
+        
+
+        // Instancia o novo produto
+        Product product = new Product(name, price);
+
+
+        // Mostra as infos na tela
+        System.out.println();
+        System.out.println("Product data: " + product);
+        System.out.println();
+
+        System.out.print("Enter the number of products to be added in stock: ");
+        quantity = sc.nextInt();
+        product.addProducts(quantity);
+
+        System.out.println();
+        System.out.println("Updated data: " + product);
+        System.out.println();
+
+        System.out.print("Enter the number of products to be removed from stock: ");
+        quantity = sc.nextInt();
+        product.removeProducts(quantity);
+
+        System.out.println();
+        System.out.println("Updated data: " + product);
+
+
+        // Termina o scanner
+        sc.close();
+    }
+}
